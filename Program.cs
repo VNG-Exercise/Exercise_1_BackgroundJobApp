37a22226b1e2b1a6338d943e810011bb7013d64b
ThaiVu <thai35.tv@gmail.com>
2024-08-24 18:29:59 +0700

    update

--- Program.cs
+++ Program.cs
@@ -1,6 +1,19 @@
 ﻿using Exercise_1_BackgroundJobApp;
 
 using var context = new ApplicationDbContext();
-var job = new BackgroundJob();
-var users = await BackgroundJob.FetchUsersForPasswordUpdateAsync(context);
-await BackgroundJob.SendEmailNotificationsAsync(users);
+
+var timer = new Timer(ExecuteJobAsync, null, TimeSpan.Zero, TimeSpan.FromSeconds(10));
+
+Console.WriteLine("Press [Enter] to exit the program...");
+Console.ReadLine();
+
+async void ExecuteJobAsync(object? state)
+{
+    Console.WriteLine("Job Starting.....");
+
+    var job = new BackgroundJob();
+    var users = await BackgroundJob.FetchUsersForPasswordUpdateAsync(context);
+    await BackgroundJob.SendEmailNotificationsAsync(users);
+
+    Console.WriteLine("Job Completed!!!");
+}
